a546f4cb712e7373b099507c1511da5cf1dd3ad8
kkimngann <ngan.nguyen.thi.kim.2005@gmail.com>
2023-05-23 18:11:19 +0700

    start driver with sauce lab

--- src/test/java/driver/DriverFactory.java
+++ src/test/java/driver/DriverFactory.java
@@ -2,6 +2,7 @@ package driver;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.MutableCapabilities;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.URL;
@@ -56,7 +57,7 @@ public class DriverFactory implements MobileCapabilityTypeEx {
             Exception exception = null;
 
             try {
-                // Desired Capabilities
+                /*// Desired Capabilities
                 DesiredCapabilities desiredCaps = new DesiredCapabilities();
                 desiredCaps.setCapability(PLATFORM_NAME, "Android");
                 desiredCaps.setCapability(AUTOMATION_NAME, "uiautomator2");
@@ -70,6 +71,26 @@ public class DriverFactory implements MobileCapabilityTypeEx {
                 switch (platform){
                     case android:
                         appiumDriver = new AndroidDriver<MobileElement>(appiumServer, desiredCaps);
+                        appiumDriver.manage().timeouts().implicitlyWait(1L, TimeUnit.SECONDS);
+                        break;
+                }*/
+                MutableCapabilities caps = new MutableCapabilities();
+                caps.setCapability("platformName", "Android");
+                caps.setCapability("appium:app", "storage:filename=Android-NativeDemoApp-0.4.0.apk");  // The filename of the mobile app
+                caps.setCapability("appium:deviceName", "Google Pixel 4 GoogleAPI Emulator");
+                caps.setCapability("appium:deviceOrientation", "portrait");
+                caps.setCapability("appium:platformVersion", "13.0");
+                caps.setCapability("appium:automationName", "UiAutomator2");
+                MutableCapabilities sauceOptions = new MutableCapabilities();
+                sauceOptions.setCapability("username", "oauth-nganntk-9119c");
+                sauceOptions.setCapability("accessKey", "3d0c4fc1-023b-47c0-8e3a-13c97e8c921b");
+                sauceOptions.setCapability("build", "appium-build-MSXNW");
+                sauceOptions.setCapability("name", "First test");
+                caps.setCapability("sauce:options", sauceOptions);
+                switch (platform){
+                    case android:
+                        URL url = new URL("https://ondemand.us-west-1.saucelabs.com:443/wd/hub");
+                        appiumDriver = new AndroidDriver<MobileElement>(url, caps);
                         appiumDriver.manage().timeouts().implicitlyWait(1L, TimeUnit.SECONDS);
                         break;
                 }
